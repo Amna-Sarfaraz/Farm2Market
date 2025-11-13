f5d828943c091300ce4cac970a992d6cad8f5ae5
Amna <mohammadamna54@gmail.com>
2025-11-13 23:30:35 +0500

    Initial commit - Farm2Market project

--- market/cpp_algos/sort.cpp
+++ market/cpp_algos/sort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 struct Farmer {
@@ -11,10 +10,56 @@ struct Farmer {
     int price;
 };
 
+// Comparator function
+bool compareFarmers(const Farmer &a, const Farmer &b, int choice, int order) {
+    bool res = false;
+    if (choice == 1) res = a.price < b.price;       // Sort by price
+    else if (choice == 2) res = a.crop < b.crop;    // Sort by crop
+    else if (choice == 3) res = a.region < b.region; // Sort by region
+    return (order == 1) ? res : !res; // Ascending or descending
+}
+
+// Merge two halves
+void merge(vector<Farmer> &farmers, int left, int mid, int right, int choice, int order) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<Farmer> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = farmers[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = farmers[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (compareFarmers(L[i], R[j], choice, order))
+            farmers[k++] = L[i++];
+        else
+            farmers[k++] = R[j++];
+    }
+
+    while (i < n1)
+        farmers[k++] = L[i++];
+    while (j < n2)
+        farmers[k++] = R[j++];
+}
+
+// Merge Sort algorithm
+void mergeSort(vector<Farmer> &farmers, int left, int right, int choice, int order) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(farmers, left, mid, choice, order);
+        mergeSort(farmers, mid + 1, right, choice, order);
+        merge(farmers, left, mid, right, choice, order);
+    }
+}
+
 int main() {
     int choice, order;
 
-    // Read inputs from stdin (sent by Django)
+    // Read inputs from Django
     cin >> choice >> order;
 
     vector<Farmer> farmers = {
@@ -23,16 +68,10 @@ int main() {
         {"Sana", "Rice", "Balochistan", 150}
     };
 
-    auto cmp = [&](Farmer &a, Farmer &b) {
-        bool res = false;
-        if (choice == 1) res = a.price < b.price;
-        else if (choice == 2) res = a.crop < b.crop;
-        else if (choice == 3) res = a.region < b.region;
-        return order == 1 ? res : !res;
-    };
+    // Perform merge sort
+    mergeSort(farmers, 0, farmers.size() - 1, choice, order);
 
-    sort(farmers.begin(), farmers.end(), cmp);
-
+    // Output result for Django
     for (auto &f : farmers)
         cout << f.name << "," << f.crop << "," << f.region << "," << f.price << endl;
 
